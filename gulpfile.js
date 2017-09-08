f1300000791e649d93bd56f20d20d9e73e62ae01
kelvin-wahome <wahomekelving@gmail.com>
2017-09-08 17:36:12 +0300

    installed gulpand wrote a gulptask

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+
+gulp.task('myTask', function(){
+  console.log('hello gulp');
+});
