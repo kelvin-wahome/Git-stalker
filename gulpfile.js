53d9dcfff362b743c1c55d0b6fcf9561b93d8648
kelvin-wahome <wahomekelving@gmail.com>
2017-09-08 18:13:14 +0300

    created a gulptask to use concat package

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/gitstalker.js'] })
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
 });
